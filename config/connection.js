4bc850dd7f227a48890edbe7abe06203df933f27
skish1987 <seankish1@gmail.com>
2019-08-07 23:40:07 -0700

    connection

---
+++ config/connection.js
@@ -0,0 +1,12 @@
+var mysql = require("mysql");
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection= mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: 'root',
+        password:'Bobvilla1',
+        database:"burgers_db"
+    })
+}
